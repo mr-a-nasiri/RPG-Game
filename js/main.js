3b5b3ec7459edbc01afdeb88268ef9acc42208e3
mr-a-nasiri <mrfinal21@gmail.com>
2022-11-11 14:35:29 +0330

    updating assets

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log(123);
+/*
+`<div class="dice">${num}</div>`).join("")
+        return `<div class="health-bar-outer">
+                    <div class="health-bar-inner ${percent < 26 ? "danger" : ""}" 
+                            style="width:${percent}%;">
+                    </div>
+                </div>`
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
+                <div class="dice-container">
+                    ${diceHtml}
+                </div>
+            </div>`
+                            <div class="end-game">
+                    <h2>Game Over</h2> 
+                    <h3>${endMessage}</h3>
+                    <p class="end-emoji">${endEmoji}</p>
+                </div>
+                  `<div class="placeholder-dice"></div>`
+*/
